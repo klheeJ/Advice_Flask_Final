a2868496299b31d7602312bbead1d26bb2be584d
Klhee Jacobs <bangklhee@gmail.com>
2023-05-02 16:13:13 -0500

    user_ID update

--- models.py
+++ models.py
@@ -9,17 +9,19 @@ db = SQLAlchemy()
 class Advice(db.Model):
     id = db.Column(db.String, primary_key = True)
     advice = db.Column(db.String(500), nullable=False)
+    user_ID = db.Column(db.String(300), nullable=False)
 
-    def __init__(self,advice,id=''):
+    def __init__(self,advice,id='', user_ID=''):
         self.id = self.set_id()
         self.advice = advice
+        self.user_ID = user_ID
 
     def set_id(self):
         return str(uuid.uuid4())
 
 class AdviceSchema(ma.Schema):
     class Meta:
-        fields = ['id', 'advice']
+        fields = ['id', 'advice', 'user_ID']
 
 advice_schema = AdviceSchema()
 advices_schema = AdviceSchema(many=True)
